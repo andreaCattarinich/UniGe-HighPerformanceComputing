cd5eb07328c755e6059b154526650155c2f88281
andreaCattarinich <andy.cattarinich@gmail.com>
2025-11-17 17:51:21 +0100

    Parallel: omp1 execution time analysis

--- homework1/metrics/scaling.sh
+++ homework1/metrics/scaling.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
 # Nome del binario da eseguire
-EXEC=../build/sequential_parallel_code_identification
+BUILD=../build/
+NAME=omp1
+EXEC="$BUILD""$NAME"
 
 # Numero di esecuzioni per ciascun test
-RUNS=10
+RUNS=5
 
 # Numero massimo di thread
-MAX_THREADS=1
+MIN_THREADS=1
+MAX_THREADS=32
 
 # File di output
-OUTFILE=sequential_parallel_code_identification.txt
+OUTFILE="$NAME"_"$RUNS"runs_"$MIN_THREADS"-"$MAX_THREADS"threads.txt
 
 # Svuoto il file
 : > "$OUTFILE"
 
-echo "Running $RUNS time(s) scaling from 1 to $MAX_THREADS thread(s)..."
+echo "Running $RUNS time(s) scaling from $MIN_THREADS to $MAX_THREADS thread(s)..."
 echo "Executable -> $EXEC"
 echo "Output -> $OUTFILE"
 
-for run in $(seq 1 $RUNS); do
-    for threads in $(seq 1 $MAX_THREADS); do
+for threads in $(seq $MIN_THREADS $MAX_THREADS); do
+    for run in $(seq 1 $RUNS); do
         echo "- Run $run | $threads threads(s)..."
         OMP_NUM_THREADS="$threads" "$EXEC" >> "$OUTFILE"
         echo >> "$OUTFILE"
     done
-    echo "-----" >> "$OUTFILE"
 done
 
 echo "Done"
