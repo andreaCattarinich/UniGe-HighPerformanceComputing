7031f19a6c301598d5d76e92968d469e847257d7
Andrea Cattarinich <andy.cattarinich@gmail.com>
2025-11-23 22:03:20 +0100

    Parallelization with CUDA performed successfully

--- homework2/src/heat.c
+++ homework2/src/heat.c
@@ -70,8 +70,8 @@ int main()
   int nstep = 200; // number of time steps
 
   // Specify our 2D dimensions
-  const int ni = 1000;
-  const int nj = 1000;
+  const int ni = 10000;
+  const int nj = 10000;
   float tfac = 8.418e-5; // thermal diffusivity of silver
 
   float *temp1_ref, *temp2_ref, *temp1, *temp2, *temp_tmp;
