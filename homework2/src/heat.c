0fd2831ff8f6e4f5a12ccaffe30ab53e945cc349
Andrea Cattarinich <andy.cattarinich@gmail.com>
2025-11-24 18:05:15 +0100

    Parallelization (to be completed)

--- homework2/src/heat.c
+++ homework2/src/heat.c
@@ -76,8 +76,8 @@ int main()
   int nstep = 200; // number of time steps
 
   // Specify our 2D dimensions
-  const int ni = 20000;
-  const int nj = 20000;
+  const int ni = 15000;
+  const int nj = 15000;
   float tfac = 8.418e-5; // thermal diffusivity of silver
 
   float *temp1_ref, *temp2_ref, *temp1, *temp2, *temp_tmp;
