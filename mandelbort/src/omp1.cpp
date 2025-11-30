9259c81bb8d815caeb5bd4e72b43fecf832b0769
andreacattarinich <andy.cattarinich@gmail.com>
2025-11-30 21:36:39 +0100

    Ready for OMP tests

--- mandelbort/src/omp1.cpp
+++ mandelbort/src/omp1.cpp
@@ -13,7 +13,7 @@
 #define RATIO_Y (MAX_Y - MIN_Y) // 2
 
 // Image size
-#define RESOLUTION 1000
+#define RESOLUTION 2000
 #define WIDTH (RATIO_X * RESOLUTION)
 #define HEIGHT (RATIO_Y * RESOLUTION)
 
@@ -25,16 +25,16 @@
 using namespace std;
 
 int main(int argc, char **argv)
-{   
-    const auto start_entire = chrono::steady_clock::now();
-    
+{
     cout << "RESOLUTION   = " << RESOLUTION << endl;
     cout << "ITERATION    = " << ITERATIONS << endl;
     cout << "HEIGHT*WIDTH = " << HEIGHT*WIDTH << endl;
     
     int *const image = new int[HEIGHT * WIDTH];
 
-    const auto start_par = chrono::steady_clock::now();
+    const auto start = chrono::steady_clock::now();
+    
+    #pragma omp parallel for
     for (int pos = 0; pos < HEIGHT * WIDTH; pos++)
     {
         image[pos] = 0;
@@ -61,22 +61,11 @@ int main(int argc, char **argv)
             }
         }
     }
-    const auto end_par = chrono::steady_clock::now();    
+    const auto end = chrono::steady_clock::now();
+    cout << "Time elapsed: "
+         << chrono::duration_cast<chrono::seconds>(end - start).count()
+         << " seconds." << endl;
 
     delete[] image;
-    const auto end_entire = chrono::steady_clock::now();    
-
-    auto Ts = chrono::duration_cast<chrono::milliseconds>(end_entire - start_entire).count() -
-                   chrono::duration_cast<chrono::milliseconds>(end_par - start_par).count();
-    
-    auto Tp = chrono::duration_cast<chrono::milliseconds>(end_par - start_par).count();
-    auto Ttot = chrono::duration_cast<chrono::milliseconds>(end_entire - start_entire).count();
-    
-    double Fs = (double)Ts / Ttot;
-    double Fp = (double)Tp / Ttot;
-    
-    cout << "Tp = " << Tp << " ms | Fp = " << Fp << endl;
-    cout << "Ts = " << Ts << " ms | Fs = " << Fs << endl;
-
     return 0;
 }
--- mandelbort/src/omp2.cpp
+++ mandelbort/src/omp2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <complex>
 #include <chrono>
 
 // Ranges of the set
@@ -10,11 +9,11 @@
 #define MAX_Y 1
 
 // Image ratio
-#define RATIO_X (MAX_X - MIN_X)
-#define RATIO_Y (MAX_Y - MIN_Y)
+#define RATIO_X (MAX_X - MIN_X) // 3
+#define RATIO_Y (MAX_Y - MIN_Y) // 2
 
 // Image size
-#define RESOLUTION 1000
+#define RESOLUTION 2000
 #define WIDTH (RATIO_X * RESOLUTION)
 #define HEIGHT (RATIO_Y * RESOLUTION)
 
@@ -27,67 +26,46 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    cout << "RESOLUTION   = " << RESOLUTION << endl;
+    cout << "ITERATION    = " << ITERATIONS << endl;
+    cout << "HEIGHT*WIDTH = " << HEIGHT*WIDTH << endl;
+    
     int *const image = new int[HEIGHT * WIDTH];
 
     const auto start = chrono::steady_clock::now();
+    
+    #pragma omp parallel for schedule(guided)
     for (int pos = 0; pos < HEIGHT * WIDTH; pos++)
     {
         image[pos] = 0;
 
         const int row = pos / WIDTH;
         const int col = pos % WIDTH;
-        const complex<double> c(col * STEP + MIN_X, row * STEP + MIN_Y);
+
+        double cRe = col * STEP + MIN_X;
+        double cIm = row * STEP + MIN_Y;
 
         // z = z^2 + c
-        complex<double> z(0, 0);
+        double zRe = 0, zIm = 0;
         for (int i = 1; i <= ITERATIONS; i++)
         {
-            z = z*z + c;
+            double oldRe = zRe;
+            zRe = (zRe*zRe - zIm*zIm) + cRe;
+            zIm = 2*oldRe*zIm + cIm;
 
             // If it is convergent
-            if (abs(z) >= 2)
+            if(zRe*zRe + zIm*zIm >= 4)
             {
                 image[pos] = i;
                 break;
             }
         }
     }
-
     const auto end = chrono::steady_clock::now();
     cout << "Time elapsed: "
          << chrono::duration_cast<chrono::seconds>(end - start).count()
          << " seconds." << endl;
 
-    // Write the result to a file
-    ofstream matrix_out;
-
-    if (argc < 2)
-    {
-        cout << "Please specify the output file as a parameter." << endl;
-        return -1;
-    }
-
-    matrix_out.open(argv[1], ios::trunc);
-    if (!matrix_out.is_open())
-    {
-        cout << "Unable to open file." << endl;
-        return -2;
-    }
-
-    for (int row = 0; row < HEIGHT; row++)
-    {
-        for (int col = 0; col < WIDTH; col++)
-        {
-            matrix_out << image[row * WIDTH + col];
-
-            if (col < WIDTH - 1)
-                matrix_out << ',';
-        }
-        if (row < HEIGHT - 1)
-            matrix_out << endl;
-    }
-    matrix_out.close();
-
-    delete[] image; // It's here for coding style, but useless
+    delete[] image;
     return 0;
 }
